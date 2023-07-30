3f85502abae50a32985195403c6981a7d4e6e01f
Enielect <eniolafaithful57@gmail.com>
2023-07-30 03:08:01 +0100

    implement polling

--- public/script.js
+++ public/script.js
@@ -288,3 +288,25 @@ function renderTalkForm(dispatch) {
         elt('label', null, 'Summary: ', summary),
         elt('button', {type: 'submit'}, 'Submit'));
 }
+
+//writing a function that keeps polling the server for talks
+//and calls a callback function when a new set of talks is available
+async function pollTalks(update) {
+    let tag = undefined;
+    for(;;) {
+        let response;
+        try {
+            response = await fetchOK('/talks', {
+                headers: tag && {'If-None-Match': tag,
+                                'Prefer': 'wait=90'}
+            });
+        } catch(e) {
+            console.log('Request failed: ' + e);
+            await new Promise(resolve => setTimeout(resolve, 500));
+            continue;
+        }
+        if(response.status == 304) continue;
+        tag = response.headers.get('ETag');
+        update(await response.json());
+    }
+}
