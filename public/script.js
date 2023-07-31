c14472d744df1010d8397e37c3a88cdbf3a06e59
Enielect <eniolafaithful57@gmail.com>
2023-07-31 16:56:38 +0100

    tie the whole user interface together

--- public/script.js
+++ public/script.js
@@ -310,3 +310,28 @@ async function pollTalks(update) {
         update(await response.json());
     }
 }
+
+//the application
+//the following component ties the whole user interface together
+
+class SkillShareApp {
+    constructor(state, dispatch) {
+        this.dispatch = dispatch;
+        this.talkDOM = elt('div', {className: 'talks'});
+        this.dom = elt('div', null,
+                        renderUserField(state.user, dispatch),
+                        this.talkDOM,
+                        renderTalkForm(dispatch));
+        this.syncState(state);
+    }
+    syncState(state) {
+        if(state.talks != this.talks) {
+            this.talkDOM.textContent = '';
+            for(let talk of state.talks) {
+                this.talkDOM.appendChild(
+                    renderTalk(talk, this.dispatch));
+            }
+            this.talks = state.talks;
+        }
+    }
+}
